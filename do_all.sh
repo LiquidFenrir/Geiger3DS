00d32c144ed4fcd3586edd50c7d9ea3cb9e2a890
Théo B. <liquidfenrir@outlook.fr>
2024-06-22 16:30:10 +0200

    make do_all more flexible

--- do_all.sh
+++ do_all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo "bins in folder $1"
+echo "output name $2"
 set -e -x
 ./do_compile_capstone.sh
 ./do_build_recompiler.sh
-./recompiler.exe ./code.bin ./rodata.bin ./data.bin outtest > outtest.dump.txt
+./recompiler.exe "$1/code.bin"  "$1/rodata.bin"  "$1/data.bin" "$2" > "$2.dump.txt"
 ./do_step_compile_child.sh outtest
